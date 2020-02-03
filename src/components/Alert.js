c84613508c8bb649417dfd11111d179a0f165cbb
mikayel5 <mikayelnahapetyan@gmail.com>
2020-02-03 12:32:47 +0400

    implement functionality of component Alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AlertContext } from '../context/alert/alerContext'
 
 
-const Alert = ({ alert }) => {
+const Alert = () => {
+
+    const { alert, hide } = useContext(AlertContext)
+    if (!alert) return null
+
+
     return (
         <div
             className={`alert alert-${alert.type || 'secondary'} alert-dismissible`}
             role="alert">
             {alert.text}
-            <button type="button" className="close" aria-label="Close">
+            <button type="button" className="close" aria-label="Close" onClick={hide}>
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AlertContext } from '../context/alert/alerContext'
 
 
 const Search = () => {
+    const { show } = useContext(AlertContext)
+
+    const onSubmit = event => {
+        if (event.key === 'Enter') {
+            show('This is alert')
+        }
+    }
+
     return (
         <div className='form-group'>
             <input
                 type='text'
                 className='form-control'
                 placeholder='write nik User...'
+                onKeyPress={onSubmit}
             />
         </div>
     )
